fb08bd1d2e8582c33f502d126aad1bfd5ac8c29b
Luis1grfe <luismendezmor@outlook.com>
2020-07-29 16:48:25 -0400

    Domain Generator

---
+++ app.js
@@ -0,0 +1,16 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let domain =['.com','.cl','.us','.me','.co','.info'];
+function generator(){
+    
+   var pronounAux = Math.floor(Math.random() * pronoun.length);
+   var adjAux = Math.floor(Math.random() * adj.length);
+   var nounAux = Math.floor(Math.random() * noun.length);
+   var domainAux = Math.floor(Math.random() * domain.length);
+   console.log(pronounAux+" "+adjAux+" "+nounAux+" "+domainAux);
+   var print = console.log(pronoun[pronounAux]+adj[adjAux]+noun[nounAux]+domain[domainAux]);
+
+   return print;
+}
+generator();
